7291c0d3d961b7e0bb5b94d3e3ba9ae1596b1d48
Eleasah Halsmer <eleasahjh@gmail.com>
2019-12-06 10:27:31 -0700

    Wip reading csv in cityreader.py

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,5 +1,16 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+import csv
+
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+    def __str__(self):
+        return f"City: {self.name}"
+    def __repr__(self):
+        return f"City({repr(self.name, self.lat, self.lon)})"
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -20,7 +31,12 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+    citiesFile = open('cities.csv')
+    citiesReader = csv.reader(citiesFile)
+    citiesData = list(citiesReader)
+    # print('number of rows: ', len(citiesData))
+    for i in range(1,61):
+        print(citiesData[i][0])
     return cities
 
 cityreader(cities)
